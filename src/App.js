18f8c672ea72ea68bbda184cb8325ccd482a1864
akim-dev <78294519+akim-dev@users.noreply.github.com>
2023-09-22 22:36:14 +0700

    1 file modified

--- src/App.js
+++ src/App.js
@@ -7,6 +7,23 @@ export default function App() {
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
+      <form>
+        <h2 className="label-wrapper">
+          <label htmlFor="new-todo-input" className="label__lg">
+            What needs to be done?
+          </label>
+        </h2>
+        <input
+          type="text"
+          id="new-todo-input"
+          className="input input__lg"
+          name="text"
+          autoComplete="off"
+        />
+        <button type="submit" className="btn btn__primary btn__lg">
+          Add
+        </button>
+      </form>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
