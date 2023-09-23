e97cfb9f29ac6e113431b9c6b42c6e3b73866c7e
akim-dev <78294519+akim-dev@users.noreply.github.com>
2023-09-24 02:05:58 +0700

    2 files modified

--- src/App.js
+++ src/App.js
@@ -5,13 +5,18 @@ import Todo from './components/Todo';
 import Form from './components/Form';
 
 export default function App({ data }) {
+  // untuk callback yaitu passing data dari child ke APP melalui props
+  function addTask(name) {
+    return console.log('ini dari add task function yang ada di APP');
+  }
+
   const taskList = data.map((todo) => (
     <Todo name={todo.name} completed={todo.completed} />
   ));
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <Form />
+      <Form addTodo={addTask} />
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
